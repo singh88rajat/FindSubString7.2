9a5a260c8b32506bfad9e5adb2a94ca65ecba7a8
Mukesh Kumar <singh88rajat@gmail.com>
2017-02-10 12:40:01 +0530

    FindSubString7.2

---
+++ src/FindSubString.java
@@ -0,0 +1,10 @@
+
+public class FindSubString {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String inputstring = "Java is better than C++";
+		
+		System.out.println("Sub String is : "+inputstring.substring(0, 10));
+		
+	}
+}
